34b9cb799196ac7bbdd5de58309b4d094486c14c
Saptarshi Ghosh <30474432+sghoshjr@users.noreply.github.com>
2025-10-17 20:16:50 +0000

    Initial Template

---
+++ eslint.config.ts
@@ -0,0 +1,72 @@
+import css from '@eslint/css';
+import js from '@eslint/js';
+import angular from 'angular-eslint';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config([
+  {
+    ignores: [
+      'dist',
+      'projects/**/*',
+      'node_modules',
+      '.config/',
+      'dist/',
+      'tsconfig.json',
+      'tsconfig.app.json',
+      'tsconfig.spec.json',
+    ],
+  },
+
+  // TypeScript + Angular
+  {
+    files: ['**/*.ts'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      prettierRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    languageOptions: {
+      ecmaVersion: 2024,
+      sourceType: 'module',
+      globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    rules: {
+      '@angular-eslint/directive-selector': ['error', { type: 'attribute', prefix: 'app', style: 'camelCase' }],
+      '@angular-eslint/component-selector': ['error', { type: 'element', prefix: 'app', style: 'kebab-case' }],
+      '@angular-eslint/component-class-suffix': ['error', { suffixes: ['Component'] }],
+      '@angular-eslint/directive-class-suffix': ['error', { suffixes: ['Directive'] }],
+      '@angular-eslint/no-empty-lifecycle-method': 'off',
+      '@angular-eslint/use-lifecycle-interface': 'off',
+      'no-console': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      'prettier/prettier': 'error',
+    },
+  },
+
+  // HTML templates
+  {
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility, prettierRecommended],
+    rules: {
+      '@angular-eslint/template/no-negated-async': 'error',
+      'prettier/prettier': ['error', { parser: 'angular' }],
+    },
+  },
+
+  // CSS
+  {
+    files: ['**/*.css'],
+    plugins: { css },
+    language: 'css/css',
+    extends: [css.configs.recommended],
+  },
+]);

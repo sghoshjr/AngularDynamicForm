4edbde7def47728a8b8ae40bb159195d5bfc4f61
Saptarshi Ghosh <30474432+sghoshjr@users.noreply.github.com>
2025-10-21 20:55:12 +0000

    initial setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home-component/home-component';
+import { UserComponent } from './components/user-component/user-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Home',
+    component: HomeComponent,
+  },
+  {
+    path: 'user',
+    title: 'User',
+    component: UserComponent,
+  },
+];

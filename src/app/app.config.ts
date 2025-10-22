20869a2156d443e0ecfedd080ef549bccdd4165c
Saptarshi Ghosh <30474432+sghoshjr@users.noreply.github.com>
2025-10-22 15:08:25 +0000

    sort imports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,9 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
+import { provideRouter } from '@angular/router';
 import Aura from '@primeuix/themes/aura';
+import { providePrimeNG } from 'primeng/config';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [

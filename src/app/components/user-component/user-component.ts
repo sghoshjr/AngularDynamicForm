1abc38ebbe487587803173024056d5d974feef97
Saptarshi Ghosh <30474432+sghoshjr@users.noreply.github.com>
2025-10-23 00:02:45 +0000

    update tsconfig

--- src/app/components/user-component/user-component.ts
+++ src/app/components/user-component/user-component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { RadioButtonModule } from 'primeng/radiobutton';
 
 @Component({
   selector: 'app-user-component',
-  imports: [ButtonModule, CommonModule],
+  imports: [ButtonModule, CommonModule, RadioButtonModule],
   templateUrl: './user-component.html',
   styleUrl: './user-component.scss',
 })
